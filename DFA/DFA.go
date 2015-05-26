279fdaf47b1048abb5ee781eae6fd5f1cd22a410
hhh0pE <vlad@lesnoy.name>
2015-05-26 22:25:21 +0300

    Another not complete realization.

--- DFA/DFA.go
+++ DFA/DFA.go
@@ -10,13 +10,13 @@ type DFA struct {
 	nodes      []*Node
 }
 
-func isArraysEqual(arr []int, arr2 []int) bool {
+func isIntArraysEqual(arr []int, arr2 []int) bool {
     if len(arr) != len(arr2) {
         return false
     }
 
     for _, val := range arr {
-        if !inArray(arr2, val) {
+        if !inIntArray(arr2, val) {
             return false
         }
     }
@@ -24,12 +24,22 @@ func isArraysEqual(arr []int, arr2 []int) bool {
     return true
 }
 
-func inArray(arr []int, num int) bool {
+func inIntArray(arr []int, num int) bool {
     for _, val := range arr {
         if val == num {
             return true
         }
     }
+    return false
+}
+
+func inStringArray(arr []string, str string) bool {
+    for _, val := range arr {
+        if val == str {
+            return true
+        }
+    }
+
     return false
 }
 
@@ -39,14 +49,14 @@ func walkFrom(node *NFA.Node, ids *[]int, symbol string) {
     }
     if node.Left != nil && (node.LeftSymbol == "" || node.LeftSymbol == symbol) {
 //        fmt.Println("left if")
-        if inArray(*ids, node.Left.Id) {
+        if inIntArray(*ids, node.Left.Id) {
            return
         }
         *ids = append(*ids, node.Left.Id)
         walkFrom(node.Left, ids, symbol)
     }
     if node.Right != nil && (node.RightSymbol == "" || node.RightSymbol == symbol){
-        if inArray(*ids, node.Right.Id) {
+        if inIntArray(*ids, node.Right.Id) {
             return
         }
         *ids = append(*ids, node.Right.Id)
@@ -61,33 +71,46 @@ func NewFromNFA (nfa *NFA.NFA) *DFA {
         Symbols map[string][]int
     }
 
-    string_to_int := make(map[string][]int)
-    string_to_int["1,2,3,4,8,9,11,12"] = []int{1,2,3,4,8,9,11,12}
+    NodeBucketIds := make(map[int][]int)
 
-    symbols := make(map[string]bool)
 
-    dfa_nodes := make(map[string]map[string]string)
-    dfa_nodes["1,2,3,4,5"] = make(map[string]string)
-    dfa_nodes["1,2,3,4,5"]["a"] = "1,2,3,4"
-    fmt.Println(dfa_nodes)
+    var dfa_nodes_count int
+
+    var symbols []string
 
     for _, node := range nfa.Nodes() {
-        if node.LeftSymbol != "" {
-            symbols[node.LeftSymbol] = true
+        if node.LeftSymbol!="" && !inStringArray(symbols, node.LeftSymbol) {
+            symbols = append(symbols, node.LeftSymbol)
         }
-        if node.RightSymbol != "" {
-            symbols[node.RightSymbol] = true
+        if node.RightSymbol!="" && !inStringArray(symbols, node.RightSymbol) {
+            symbols = append(symbols, node.RightSymbol)
         }
     }
 
-    fmt.Println(symbols)
+//    nodes := make(map[int]map[string]int)
 
-    for _, node := range nfa.Nodes() {
-        var nodes []int
-        nodes = append(nodes, node.Id)
-        walkFrom(node, &nodes, "")
-        fmt.Println(nodes)
-    }
+    var tmp_nodes []int
+    tmp_nodes = append(tmp_nodes, nfa.Nodes()[0].Id)
+    walkFrom(nfa.Nodes()[0], &tmp_nodes, "")
+    NodeBucketIds[dfa_nodes_count] = tmp_nodes
+    dfa_nodes_count++
+
+    fmt.Println(NodeBucketIds)
+
+//    for _, node := range nfa.Nodes() {
+//
+//    }
+//
+//    var nodes []int
+//    walkFrom(nfa.Nodes()[0], &nodes, "")
+//    fmt.Printf("%+v\n", nodes)
+
+//    for _, node := range nfa.Nodes() {
+//        var nodes []int
+//        nodes = append(nodes, node.Id)
+//        walkFrom(node, &nodes, "")
+//        fmt.Printf("%+v: %+v\n", node, nodes)
+//    }
 //    tmp_nodes :=  make([]NFATemp, nfa.Length())
 //    for i,_ := range tmp_nodes {
 //        tmp_nodes[i].Symbols = make(map[string][]int)
